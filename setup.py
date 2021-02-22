2f88e2f2908ca8d481e8be028722fe25e22830b4
patlewig <patlewig@users.noreply.github.com>
2021-02-22 17:55:51 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applying a proof of principle information approach to assessing genotoxicity for TSCA chemicals.',
+    author='patlewig',
+    license='MIT',
+)
